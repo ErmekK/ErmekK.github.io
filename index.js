8dabd3f0d6212dd6f80b8b466cc59d6be0d8a93b
ErmekK <53251396+ErmekK@users.noreply.github.com>
2020-04-22 08:13:24 +0600

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+let stpbl1 = document.getElementById("stepbl1");
+let stpbrd1 = document.getElementById("step-border1");
+
+let stpbl3 = document.getElementById("stepbl3");
+let stpbrd3 = document.getElementById("step-border3");
+
+let stpbl2 = document.getElementById("stepbl2");
+let stpbrd2 = document.getElementById("step-border2");
+
+
+stpbl1.onmouseout = function(){
+    stpbrd1.style.borderBottom = "3px solid #000";
+}
+
+stpbl2.onmouseover = function(){
+    stpbrd2.style.borderBottom = "3px solid #fff";
+}
+stpbl2.onmouseout = function(){
+    stpbrd2.style.borderBottom = "3px solid #000";
+}
+
+stpbl3.onmouseover = function(){
+    stpbrd3.style.borderBottom = "3px solid #fff";
+}
+stpbl3.onmouseout = function(){
+    stpbrd3.style.borderBottom = "3px solid #000";
+}
